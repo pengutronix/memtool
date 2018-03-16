pengutronix/memtool

commit a7e2f45e6d8eb6658ca28ead9192f6f384db40b1

Implement a -V switch to get the version

memtool.c
@@ -479,6 +479,11 @@ int main(int argc, char **argv)
 	if (!strcmp(basename(argv[0]), "memtool")) {
 		argv++;
 		argc--;
+
+		if (!strcmp(argv[0], "-V")) {
+			printf("%s\n", PACKAGE_STRING);
+			return EXIT_SUCCESS;
+		}
 	}
 
 	if (argc < 1) {

memtool.1
@@ -2,6 +2,8 @@
 .SH NAME
 memtool \- display and modify memory
 .SH SYNOPSIS
+.B memtool -V
+.br
 .B memtool md
 .RB [\| \-b \||\| \-w \||\| \-l \||\| \-q \|]
 .RB [\| \-x \|]
@@ -34,6 +36,11 @@ This might result in all kind of errors which are not considered to be a bug of
 the tool, but either the machine or the caller.
 
 .SH OPTIONS
+.TP
+.B \-V
+Dump memtool version and exit
+
+.SH COMMON OPTIONS FOR SUBCOMMANDS
 .TP
 .B \-b
 Use byte wise (8-bit) access

commit a9e244d4da0f1153381b9410b2c7eada6d6ef52c

Fix segfault when memtool is called without arguments
Fixes: a7e2f45e6d8e ("Implement a -V switch to get the version")
Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>

memtool.c
@@ -480,7 +480,7 @@ int main(int argc, char **argv)
 		argv++;
 		argc--;
 
-		if (!strcmp(argv[0], "-V")) {
+		if (argc > 0 && !strcmp(argv[0], "-V")) {
 			printf("%s\n", PACKAGE_STRING);
 			return EXIT_SUCCESS;
 		}